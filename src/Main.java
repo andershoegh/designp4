29fbd15c735256b565b4fa530d8878aa4fc3f3c4
andershoegh <ahha15@student.aau.dk>
2018-03-06 12:39:59 +0100

    ny main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Player andreas = new Player("Andreas Sigsgaard", "Søndergade 62, 2 Sal lejl 5, 9000 Aalborg",
+                "asigsg16@student.aau.dk","Hans mor", 98989898,"Angriber", "25-02-1994",
+                3, 5, 10, 0, 3, 10,
+
+                6, 0);
+
+        Player tummas = new Player("Tummas Jóhan Sigvardsen", "Godsbanen 17, 3., 22, 9000 Aalborg",
+
+                "tsigva16@student.aau.dk","Din mor", 50695073,"Defence", "22-10-1992",
+
+                4, 5, 10, 0, 3, 10,
+
+                6, 0);
+// test
+        System.out.println(andreas.getAddress());
+        System.out.println(tummas.getAddress());
+
+        Match kamp = new Match("d", 5, 2, 4, "ed",
+
+                "Ig",2);
+
+        System.out.println(kamp.getscore());
+
+
+
+
     }
+
 }
