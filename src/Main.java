f41ff8131c329ac03f112419f992617e58494269
Unknown <lassestausgaard@gmail.com>
2018-03-06 12:58:05 +0100

    fjernet overflødigt lort fra main

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,19 @@ public class Main {
         Player andreas = new Player("Andreas Sigsgaard", "Søndergade 62, 2 Sal lejl 5, 9000 Aalborg",
                 "asigsg16@student.aau.dk","Hans mor", 98989898,"Angriber", "25-02-1994",
                 3, 5, 10, 0, 3, 10,
-
                 6, 0);
-//lolleren
+
         Player tummas = new Player("Tummas Jóhan Sigvardsen", "Godsbanen 17, 3., 22, 9000 Aalborg",
-
                 "tsigva16@student.aau.dk","Din mor", 50695073,"Defence", "22-10-1992",
-
                 4, 5, 10, 0, 3, 10,
-
                 6, 0);
-// test i am masterbaiter wtf
-// Så var der historien om hans og grethe den gik jo nu engang som den altid gør
+
         System.out.println(andreas.getAddress());
         System.out.println(tummas.getAddress());
 
         Match kamp = new Match(5, 1, 2, 2, "Andreas", "AaB");
 
         System.out.println(kamp.getGoalsFor() + " - " + kamp.getGoalsAgainst());
-
-
-
-
     }
 
 }
