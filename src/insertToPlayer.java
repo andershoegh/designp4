24542a9061d24e96a16972bec3ae6e138d04e3e5
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2018-03-13 16:17:51 +0100

    Scanner -> Database
    Made it possible to input data to our database, using the Scanner method.

--- src/insertToPlayer.java
+++ src/insertToPlayer.java
@@ -1,24 +1,57 @@
 import java.sql.*;
+import java.util.Scanner;
 
-/**
- * JdbcInsert1.java - Demonstrates how to INSERT data into an SQL
- *                    database using Java JDBC.
- */
 class insertToPlayer {
 
     public static void insertPlayers(Connection conn) throws SQLException {
-        Statement stmt = null;
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            stmt = conn.createStatement();
+        System.out.print("Navn: ");
+        String name = scanner.nextLine();
 
-            stmt.executeUpdate("INSERT INTO player VALUES (null, 'Pernille', 'Finlandsgade 7, 9000 Aalborg', 31313131, 'test@test.dk', null, null, 'Bænken', 1,0,0,0,0,0,0,0)");
+        System.out.print("Addresse: ");
+        String address = scanner.nextLine();
 
-            conn.close();
-        } catch (Exception e) {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
+        System.out.print("Telefonnummer: ");
+        String phone = scanner.nextLine();
 
+        System.out.print("Email: ");
+        String mail = scanner.nextLine();
+
+        System.out.print("ICE (In Case of Emergency) navn: ");
+        String iceName = scanner.nextLine();
+
+        System.out.print("ICE (In Case of Emergency) telefonnummer: ");
+        String iceTelephone = scanner.nextLine();
+
+        System.out.print("Position: ");
+        String position = scanner.nextLine();
+
+        System.out.print("Er vedkommende skadet? (1= Nej, 2=Ja): ");
+        String health = scanner.nextLine();
+
+
+        String sql = "INSERT INTO player "
+                + " (_id, name, address, phone, mail," +
+                "iceName, iceTelephone, position, health, " +
+                "yellowCards, redCards, goalScored, assist, motm, " +
+                "attendedMatches, attendedTrainings)" +
+
+                " VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, null, null, null, null, null, null, null)";
+
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+
+        stmt.setString(1, name);
+        stmt.setString(2, address);
+        stmt.setString(3, phone);
+        stmt.setString(4, mail);
+        stmt.setString(5, iceName);
+        stmt.setString(6, iceTelephone);
+        stmt.setString(7, position);
+        stmt.setString(8, health);
+
+
+        stmt.executeUpdate();
     }
 }
