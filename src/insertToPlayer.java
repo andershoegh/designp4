bd266c7d9d1f66ee41e8162194569837cd52b7b5
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2018-03-13 16:49:41 +0100

    Auto-'null' in Scanner
    Added an if-statement under scanner, so it automatically applies 'null'

--- src/insertToPlayer.java
+++ src/insertToPlayer.java
@@ -8,29 +8,54 @@ class insertToPlayer {
 
         System.out.print("Navn: ");
         String name = scanner.nextLine();
+        if(name.equals("")){
+            name = null;
+        }
 
         System.out.print("Addresse: ");
         String address = scanner.nextLine();
+        if(address.equals("")){
+            address = null;
+        }
 
         System.out.print("Telefonnummer: ");
         String phone = scanner.nextLine();
+        if(phone.equals("")){
+            phone = null;
+        }
 
         System.out.print("Email: ");
         String mail = scanner.nextLine();
+        if(mail.equals("")){
+            mail = null;
+        }
 
         System.out.print("ICE (In Case of Emergency) navn: ");
         String iceName = scanner.nextLine();
+        if(iceName.equals("")){
+            iceName = null;
+        }
 
         System.out.print("ICE (In Case of Emergency) telefonnummer: ");
         String iceTelephone = scanner.nextLine();
+        if(iceTelephone.equals("")){
+            iceTelephone = null;
+        }
 
         System.out.print("Position: ");
         String position = scanner.nextLine();
+        if(position.equals("")){
+            position = null;
+        }
 
         System.out.print("Er vedkommende skadet? (1= Nej, 2=Ja): ");
         String health = scanner.nextLine();
+        if(health.equals("")){
+            health = "1";
+        }
 
 
+        // Prepareing to insert the values into database
         String sql = "INSERT INTO player "
                 + " (_id, name, address, phone, mail," +
                 "iceName, iceTelephone, position, health, " +
@@ -39,9 +64,10 @@ class insertToPlayer {
 
                 " VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, null, null, null, null, null, null, null)";
 
-
+        // Connecting to the database
         PreparedStatement stmt = conn.prepareStatement(sql);
 
+        // Inserting the values into the database
         stmt.setString(1, name);
         stmt.setString(2, address);
         stmt.setString(3, phone);
@@ -51,7 +77,7 @@ class insertToPlayer {
         stmt.setString(7, position);
         stmt.setString(8, health);
 
-
+        // Updates the database
         stmt.executeUpdate();
     }
 }
