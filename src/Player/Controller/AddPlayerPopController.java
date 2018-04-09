b3e140f6fbd2874da0d1ad9b611899a9b780061f
Unknown <ahha15@student.aau.dk>
2018-04-09 10:18:50 +0200

    Auto stash before merge of "master" and "origin/master"
    - Anders

--- src/Player/Controller/AddPlayerPopController.java
+++ src/Player/Controller/AddPlayerPopController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AddPlayerPopController {
 
@@ -24,14 +26,51 @@ public class AddPlayerPopController {
     @FXML
     private TextField mailInput;
     @FXML
-    private TextField ICEnameInput;
-    @FXML
-    private TextField ICEphoneInput;
+    private TextField birthdayInput;
     @FXML
     private TextField positionInput;
     @FXML
-    private TextField birthdayInput;
-    @FXML
     private TextField healthInput;
 
+    public void onOpretClick(ActionEvent event){
+
+        Connection conn = SqlConnection.connectToDB();
+        String sql = "INSERT INTO player "
+                + " (_id, name, address, phone, mail," +
+                "position, health, " +
+                "yellowCards, redCards, goalScored, assist, motm, " +
+                "attendedMatches, attendedTrainings)" +
+
+                " VALUES (null, ?, ?, ?, ?, ?, ?, null, null, null, null, null, null, null)";
+
+        // Connecting to the database
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            // Inserting the values into the database
+            stmt.setString(1, nameInput.getText());
+            stmt.setString(2, addressInput.getText());
+            stmt.setInt(3, Integer.parseInt(phoneInput.getText()));
+            stmt.setString(4, mailInput.getText());
+            stmt.setString(5, positionInput.getText());
+            stmt.setString(6, healthInput.getText());
+
+            // Updates the database
+            stmt.executeUpdate();
+
+            // Switching scene from AddPlayer.FXML to PlayerList.FXML
+            Parent addPlayerFXML = FXMLLoader.load(getClass().getResource("../PlayerList.fxml"));
+            Scene addPlayerScene = new Scene(addPlayerFXML);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(addPlayerScene);
+            stage.show();
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        SqlConnection.closeConnection();
+    }
 }
