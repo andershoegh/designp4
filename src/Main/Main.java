a7e39ee41286136757ebe90e5e707e5c8448cb62
Anders Høgh <ahha15@student.aau.dk>
2018-04-13 12:39:52 +0200

    - Anders

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,14 +12,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../Overview/Overview.fxml"));
         primaryStage.setTitle("Coach meister 9001");
-        primaryStage.setScene(new Scene(root, 1280,720));
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
+
 }
-
-
