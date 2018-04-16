224158c0fbc0a3524e62e12b7028a6cd81b96640
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2018-04-16 13:08:48 +0200

    Statistics DONE - No sorting though

--- src/Statistic/Controller/StatisticController.java
+++ src/Statistic/Controller/StatisticController.java
@@ -1,9 +1,11 @@
 package Statistic.Controller;
 
+import Controller.MenuController;
 import Player.Player;
 import SQL.SqlConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -17,7 +19,13 @@ public class StatisticController {
 
     private ObservableList<Player> playerData;
 
-    // setting FXML IDs
+    /*
+     *
+     * Setting FXML IDs
+     *
+     */
+
+    // Spillere
     @FXML
     private TableView<Player> stats_players_tableview;
     /*@FXML
@@ -28,6 +36,79 @@ public class StatisticController {
     private TableColumn<?, ?> stats_player_goals_tablecolumn;
     @FXML
     private TableColumn<?, ?> stats_player_assists_tablecolumn;
+
+    //Mål
+    @FXML
+    private TableView<Player> stats_goals_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_goals_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_goals_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_goals_amount_tablecolumn;
+
+    // Kampe spillet
+    @FXML
+    private TableView<Player> stats_match_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_match_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_match_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_match_amount_tablecolumn;
+
+    // Gule kort
+    @FXML
+    private TableView<Player> stats_yellowcards_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_yellowcards_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_yellowcards_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_yellowcards_amount_tablecolumn;
+
+    // Røde kort
+    @FXML
+    private TableView<Player> stats_redcards_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_cards_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_redcards_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_redcards_amount_tablecolumn;
+
+    // Assists
+    @FXML
+    private TableView<Player> stats_assists_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_assists_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_assists_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_assists_amount_tablecolumn;
+
+    // Man of the match
+    @FXML
+    private TableView<Player> stats_mofm_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_mofm_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_mofm_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_mofm_amount_tablecolumn;
+
+    // Træninger
+    @FXML
+    private TableView<Player> stats_trainings_tableview;
+    /*@FXML
+    private TableColumn<?, ?> stats_trainings_number_tablecolumn;*/
+    @FXML
+    private TableColumn<?, ?> stats_trainings_tablecolumn;
+    @FXML
+    private TableColumn<?, ?> stats_trainings_amount_tablecolumn;
+
+
+
 
     // Runs when FXML is loaded
     @FXML
@@ -41,8 +122,31 @@ public class StatisticController {
     private void setCellTable() {
         //stats_player_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
         stats_players_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        stats_player_goals_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("goalScored"));
-        stats_player_assists_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("assist"));
+        stats_player_goals_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("goalsScored"));
+        stats_player_assists_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("assists"));
+        //stats_goals_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_goals_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_goals_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("goalsScored"));
+        //stats_goals_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_match_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_match_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("attendedMatches"));
+        //stats_yellowcards_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_yellowcards_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_yellowcards_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("yellowCards"));
+        //stats_cards_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_redcards_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_redcards_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("redCards"));
+        //stats_assists_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_assists_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_assists_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("assists"));
+        //stats_mofm_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_mofm_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_mofm_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("motm"));
+        //stats_trainings_number_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        stats_trainings_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stats_trainings_amount_tablecolumn.setCellValueFactory(new PropertyValueFactory<>("attendedTrainings"));
+
+
     }
 
     private void loadDataFromDB() {
@@ -65,11 +169,17 @@ public class StatisticController {
         }
         // inputting retrieved data from db into table row
         stats_players_tableview.setItems(playerData);
+        stats_goals_tableview.setItems(playerData);
+        stats_match_tableview.setItems(playerData);
+        stats_yellowcards_tableview.setItems(playerData);
+        stats_redcards_tableview.setItems(playerData);
+        stats_assists_tableview.setItems(playerData);
+        stats_mofm_tableview.setItems(playerData);
+        stats_trainings_tableview.setItems(playerData);
+
 
         SqlConnection.closeConnection();
     }
-}
-
 
     // Menu buttons navigation
     MenuController controller = new MenuController();
