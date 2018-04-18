5d09d0132b414698c20b12410d61af4467bf6e5b
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2018-04-18 12:01:09 +0200

    Placeholder text for "Spillere" inputs.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*
         Parent root = FXMLLoader.load(getClass().getResource("../Overview/Overview.fxml"));
         primaryStage.setTitle("CoachDB");
         primaryStage.setScene(new Scene(root, 1280, 720));
