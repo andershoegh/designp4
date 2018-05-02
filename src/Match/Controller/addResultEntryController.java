0066df9a1d4c1330a9ef0574a82ef4b4e6e17300
BAD MOTHERFUCKER <lassestausgaard@gmail.com>
2018-05-02 11:35:07 +0100

    pushin'

--- src/Match/Controller/addResultEntryController.java
+++ src/Match/Controller/addResultEntryController.java
@@ -9,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
@@ -18,7 +19,7 @@ public class addResultEntryController {
 
     @FXML private Label labelTitle;
     @FXML private ChoiceBox<Player> choiceboxPlayer;
-    @FXML private TextField textfieldAmount;
+    @FXML private GridPane gridPane;
 
     StringConverter<Player> converter = new StringConverter<>() {
         @Override public String toString(Player object) { return object.getName(); }
@@ -33,15 +34,19 @@ public class addResultEntryController {
         switch (selectedTable) {
             case "Goals":
                 labelTitle.setText("Tilføj målscorer");
+                addTextField();
                 break;
             case "Assists":
                 labelTitle.setText("Tilføj assist");
+                addTextField();
                 break;
             case "Yellow":
                 labelTitle.setText("Tilføj gult kort");
+                addChoiceBox();
                 break;
             case "Red":
                 labelTitle.setText("Tilføj rødt kort");
+                addChoiceBox();
                 break;
             default:
                 break;
@@ -54,13 +59,37 @@ public class addResultEntryController {
         selectedTable = id;
     }
 
+    public void addTextField(){
+        TextField textFieldAmount = new TextField();
+        textFieldAmount.setPrefWidth(32.0);
+        textFieldAmount.setPrefHeight(35.0);
+
+        gridPane.add(textFieldAmount, 1, 1);
+    }
+
+    public void addChoiceBox(){
+        ChoiceBox<Integer> choiceBoxAmount = new ChoiceBox<>();
+        choiceBoxAmount.getItems().addAll(1, 2);
+        choiceBoxAmount.setMinWidth(32.0);
+        choiceBoxAmount.setPrefHeight(35.0);
+        choiceBoxAmount.getStyleClass().add("greenButton");
+
+        gridPane.add(choiceBoxAmount, 1, 1);
+    }
+
     public void addButtonClick(ActionEvent event){
-        if (choiceboxPlayer.getValue() != null && textfieldAmount.getText() != null) {
+        if (choiceboxPlayer.getValue() != null && gridPane.getChildren().get(3) != null) {
             inputMatchResultsController controller = new inputMatchResultsController();
-            controller.storeSelectedEntry(selectedTable,
-                    choiceboxPlayer.getValue(),
-                    Integer.parseInt(textfieldAmount.getText()));
+
+            if (selectedTable.equals("Goals") || selectedTable.equals("Assists")) {
+                controller.storeSelectedEntry(selectedTable,
+                        choiceboxPlayer.getValue(),
+                        Integer.parseInt(((TextField) gridPane.getChildren().get(3)).getText()));
+            } else {
+                controller.storeSelectedEntry(selectedTable, choiceboxPlayer.getValue(), Integer.parseInt(((ChoiceBox) gridPane.getChildren().get(3)).getValue().toString()));
+            }
         }
+
         Stage stage = (Stage) ((Node) event.getTarget()).getScene().getWindow();
         stage.close();
     }
