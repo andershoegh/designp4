f821427327bc08cc3810ba4b41e3d7fb938d959c
Anders Høgh <ahha15@student.aau.dk>
2018-05-11 12:51:04 +0200

    Calendar double click in tables and edit functionality
    - Anders

---
+++ src/Calendar/Controller/EditTrainingActivity.java
@@ -0,0 +1,23 @@
+package Calendar.Controller;
+
+import Training.Training;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+
+public class EditTrainingActivity {
+
+    private Training training;
+
+
+    @FXML
+    private DatePicker dateInput;
+    @FXML private TextField startTimeInput;
+    @FXML private TextField endTimeInput;
+    @FXML private Button createButton;
+
+
+}
+
